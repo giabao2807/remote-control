bf5d6d08aeca551d89a5e9fc5aade1db66274177
GiaBaone <102190252@sv1.dut.udn.vn>
2021-10-17 18:42:42 +0700

    clientInitiator 17.10.21

---
+++ src/client/ClientInitiator.java
@@ -0,0 +1,71 @@
+package client;
+
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ClientInitiator {
+	Socket socket = null;
+
+	public static void main(String[] args) {
+		String ip = JOptionPane.showInputDialog("Please enter server IP");
+		String port = JOptionPane.showInputDialog("Please enter server port");
+		new ClientInitiator(ip, Integer.parseInt(port));
+	}
+
+	public ClientInitiator(String ip, int port) {
+		Robot robot = null; // use to capture the screen
+		Rectangle rectangle = null; // use to represent screen dimensions
+
+		try {
+			System.out.println("Connecting to server.......");
+			socket = new Socket(ip, port);
+			System.out.println("Connection Established.");
+
+			// Get default screen device
+			GraphicsEnvironment gEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			GraphicsDevice gDev = gEnv.getDefaultScreenDevice();
+
+			// Get screen dimensions
+			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+			rectangle = new Rectangle();
+
+			// Prepare Robot object
+			robot = new Robot(gDev);
+
+			// drawGUI();
+			// ScreenSpyer sends screenshots of the client screen
+
+			// Serverdelegate recieves server commands and execute them
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void GUI() {
+		JFrame frame = new JFrame("Remote Admin");
+		JButton button = new JButton("Terminate");
+
+		frame.setBounds(100, 100, 150, 150);
+		frame.setDefaultCloseOperation(3);
+		frame.add(button);
+		button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		frame.setVisible(true);
+	}
+}
